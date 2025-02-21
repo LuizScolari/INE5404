36097f5bae571a6e7c2964955b557ccecd4b99ee
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2025-02-21 17:50:34 -0400

    Updating package name

--- Projetos/livremercado/model/Pessoa.java
+++ Projetos/livremercado/model/Pessoa.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ufsc.ine5404.livremercado.model;
+package Projetos.livremercado.model;
 
 /**
  *
